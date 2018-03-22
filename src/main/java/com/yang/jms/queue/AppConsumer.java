e24c47bb4b8fbdae7d09834969603cc0064f346c
yangjunlin <yangjunlin@jimistore.com>
2018-03-22 20:30:46 +0800

    test

---
+++ src/main/java/com/yang/jms/queue/AppConsumer.java
@@ -0,0 +1,46 @@
+package com.yang.jms.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class AppConsumer {
+    private static final String url="tcp://172.16.3.63:61616";
+    private static final String queueName="queue-test";
+
+    public static void main(String[] args) throws JMSException {
+
+        //1.创建ConnectionFactory
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+
+        //2.创建Connection
+        Connection connection = connectionFactory.createConnection();
+
+        //3.启动连接
+        connection.start();
+
+        //创建会话 第一个false是否处理事物
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        //5.创建一个目标
+        Destination destination = session.createQueue(queueName);
+
+        //6.创建一个
+        MessageConsumer consumer = session.createConsumer(destination);
+
+        //7.创建一个监听器
+        consumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println("接收消息"+textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //9.关闭连接
+        //connection.close();
+    }
+}
