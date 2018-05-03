190e6917bf5457fe3079fb3c5373188a9b2ddfae
yang505581644 <yangjian505581644@qq.com>
2018-05-03 13:28:39 +0800

    test 切换

---
+++ src/main/java/com/yang/jms/queue/Test.java
@@ -0,0 +1,12 @@
+package com.yang.jms.queue;
+
+/**
+ * @Author: 杨峻霖
+ * 2018-05-03 13:27
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
